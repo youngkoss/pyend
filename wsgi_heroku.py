f65948e86d5c160ae0e5b608a55464659c7ede84
Dmitry <vechorkodima@gmail.com>
2013-12-23 18:48:44 +0200

    Create wsgi_heroku.py

---
+++ wsgi_heroku.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for deploying hcar project on heroku service
+"""
+
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
